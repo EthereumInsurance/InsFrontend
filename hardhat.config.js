92cf043dcdbf159596959cab0f626349bbe2837e
John Sanford <jsanford9292@gmail.com>
2021-01-18 15:36:23 -0800

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.7.6",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: "0.7.3"
 };
-
